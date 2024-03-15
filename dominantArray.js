2017a2cb2b50a0e974cc9d1748e49a7bcea4d632
Rajat Palankar <rajatrrpalankar@gmail.com>
2024-03-15 17:19:28 +0530

    dominant array

---
+++ dominantArray.js
@@ -0,0 +1,41 @@
+function isDominantElementPresent(arr) {
+   
+   var count = {};
+   arr.forEach(ele=>{
+    if(count[ele]){
+        count[ele] +=1;
+           
+    }else{
+        count[ele] = 1;
+    }
+});
+
+var maxFrequency = 0;
+ for (var ele in count) {
+            maxFrequency = Math.max(maxFrequency, count[ele]);
+    }
+    var isDominant = Object.values(count).filter(freq => freq === maxFrequency).length === 1;
+    
+return isDominant;
+}
+
+
+
+
+function main() {
+    const t = parseInt(prompt("Enter the number of test cases:"));
+
+    for (let i = 0; i < t; i++) {
+        const n = parseInt(prompt("Enter the size of the array:"));
+        const arr = prompt("Enter the array elements separated by space:")
+            .split(" ");
+
+        if (isDominantElementPresent(arr)) {
+            console.log("YES");
+        } else {
+            console.log("NO");
+        }
+    }
+}
+
+main();
